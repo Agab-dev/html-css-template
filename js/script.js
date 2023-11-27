33dc8b339771eac7631477a8f0f1982280f958cf
Adam Alan <gameon.ahamd@gmail.com>
2023-11-27 16:10:36 +0300

    created the header, landing, and services section

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const nav = document.getElementById("nav");
+const navBtn = document.getElementById("navBtn");
+
+navBtn.addEventListener("click", function () {
+  nav.classList.toggle("hidden");
+});
+
+
